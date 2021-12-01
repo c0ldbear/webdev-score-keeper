cb2b909dcbda428beafbdd0e5c65b1416bf110f6
teddy <teddyjh@protonmail.com>
2021-12-01 12:15:42 +0100

    fix changes to score when there is no scorelimit, one winner etc.

--- app.js
+++ app.js
@@ -11,7 +11,9 @@ const btnReset = document.querySelector('#btnResetScore');
 const player1 = { score: 0 };
 const player2 = { score: 0 };
 
-let scoreLimit = 5;
+const defaultScore = -1;
+let scoreLimit = defaultScore;
+let winner = false;
 
 selection.addEventListener('change', (e) => {
 	console.log('change event on select, e:', e);
@@ -19,28 +21,32 @@ selection.addEventListener('change', (e) => {
 });
 
 btnPlayer1.addEventListener('click', (e) => {
-	console.log('+1 score to Player 1');
-
-	player1.score++;
-	scorePlayer1.innerText = player1.score;
+	if (!winner && scoreLimit > 0) {
+		console.log('+1 score to Player 1');
+		player1.score++;
+		scorePlayer1.innerText = player1.score;
+	}
 
 	if (player1.score === scoreLimit) {
-		/* set the number of rounds instead of specific '5' in the if-statement */
+		console.log('Player 1 have won!');
 		scorePlayer1.style.color = '#1a936f';
 		scorePlayer2.style.color = '#931a3e';
+		winner = true;
 	}
 });
 
 btnPlayer2.addEventListener('click', (e) => {
-	console.log('+1 score to Player 2');
-
-	player2.score++;
-	scorePlayer2.innerText = player2.score;
+	if (!winner && scoreLimit > 0) {
+		console.log('+1 score to Player 2');
+		player2.score++;
+		scorePlayer2.innerText = player2.score;
+	}
 
 	if (player2.score === scoreLimit) {
-		/* set the number of rounds instead of specific '5' in the if-statement */
+		console.log('Player 2 have won!');
 		scorePlayer2.style.color = '#1a936f';
 		scorePlayer1.style.color = '#931a3e';
+		winner = true;
 	}
 });
 
@@ -48,8 +54,12 @@ btnReset.addEventListener('click', () => {
 	scorePlayer1.style.color = '';
 	scorePlayer1.innerText = 0;
 	player1.score = 0;
+
 	scorePlayer2.style.color = '';
 	scorePlayer2.innerText = 0;
 	player2.score = 0;
+
 	selection.value = 0;
+	scoreLimit = defaultScore;
+	winner = false;
 });
