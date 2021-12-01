67b6b66acb0864b88725cabaeba92532ee56101b
teddy <teddyjh@protonmail.com>
2021-12-01 10:38:20 +0100

    git init and add code to start keeping track of scores

---
+++ app.js
@@ -0,0 +1,9 @@
+const btnPlayer1 = document.querySelector('#btnPlayer1');
+const btnPlayer2 = document.querySelector('#btnPlayer2');
+const scorePlayer1 = document.querySelector('#scorePlayer1');
+const scorePlayer2 = document.querySelector('#scorePlayer2');
+const scores = document.querySelector('#scores');
+
+btnPlayer1.addEventListener('click', (e) => {
+	console.log(scorePlayer1);
+});
