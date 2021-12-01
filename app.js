d0420eaf483ad4ec33d58ae5b6f8270062342678
teddy <teddyjh@protonmail.com>
2021-12-01 11:05:52 +0100

    add logic to score buttons (add score to resp players)

--- app.js
+++ app.js
@@ -4,6 +4,31 @@ const scorePlayer1 = document.querySelector('#scorePlayer1');
 const scorePlayer2 = document.querySelector('#scorePlayer2');
 const scores = document.querySelector('#scores');
 
+const player1 = { score: 0 };
+const player2 = { score: 0 };
+
 btnPlayer1.addEventListener('click', (e) => {
-	console.log(scorePlayer1);
+	console.log('+1 score to Player 1');
+
+	player1.score++;
+	scorePlayer1.innerText = player1.score;
+
+	if (player1.score === 5) {
+		/* set the number of rounds instead of specific '5' in the if-statement */
+		scorePlayer1.style.color = '#1a936f';
+		scorePlayer2.style.color = '#931a3e';
+	}
+});
+
+btnPlayer2.addEventListener('click', (e) => {
+	console.log('+1 score to Player 2');
+
+	player2.score++;
+	scorePlayer2.innerText = player2.score;
+
+	if (player2.score === 5) {
+		/* set the number of rounds instead of specific '5' in the if-statement */
+		scorePlayer2.style.color = '#1a936f';
+		scorePlayer1.style.color = '#931a3e';
+	}
 });
